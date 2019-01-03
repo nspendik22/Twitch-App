011d245f5cbd67dcf2d03247c1edd68b37243b1f
Nikolas Spendik <niks_wake@hotmail.com>
2019-01-03 15:35:25 -0500

    Updates

---
+++ precache-manifest.897783b3b2803d1126a53be45349d4a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "196b4f3af87d2c1ae249",
+    "url": "/Twitch-App/static/js/runtime~main.196b4f3a.js"
+  },
+  {
+    "revision": "cf8897355f346e85652f",
+    "url": "/Twitch-App/static/js/main.cf889735.chunk.js"
+  },
+  {
+    "revision": "b5d12cf8bac00105ad44",
+    "url": "/Twitch-App/static/js/1.b5d12cf8.chunk.js"
+  },
+  {
+    "revision": "cf8897355f346e85652f",
+    "url": "/Twitch-App/static/css/main.f15bd5bf.chunk.css"
+  },
+  {
+    "revision": "98c385535b26e5b3fee870b4a099c8f8",
+    "url": "/Twitch-App/index.html"
+  }
+];
